b4fa09e963dd73d0401aadd3e09b65f6ba309a69
Kevin Samoei <samoeikev@gmail.com>
2018-09-04 10:24:27 +0300

    Add and configure Rspec

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,12 @@ module Profile
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # config.generators.test_framework false
+
+    config.generators do |g|
+      g.test_framework  :rspec, :fixture => false
+    end
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
